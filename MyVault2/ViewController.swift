f1692d087c742693fddae636ec1e24953b2e33fb
SSIL <SSIL_UMS@ssils-macbook-air.local>
2016-08-08 14:35:36 +0800

    Initial Commit

---
+++ MyVault2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MyVault2
+//
+//  Created by SSIL on 8/8/16.
+//  Copyright © 2016 Fadhli. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
